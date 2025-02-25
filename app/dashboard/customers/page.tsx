964a9114628237176eeb2cf466f5fcd3b4e93629
Gianpierre Fernandez <gianpierrefernandez@gianpierres-macbook-pro.home>
2025-02-25 11:24:32 +0100

    feat: add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,23 @@
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
 import { Metadata } from "next";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { Suspense } from "react";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
   title: "Customers | Acme Dashboard",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    search?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.search || "";
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return <CustomersTable customers={customers} />;
 }
